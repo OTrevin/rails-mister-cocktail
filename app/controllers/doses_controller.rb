8da52fb9a1eebfb13c151af63a0bbd1d46b61e3f
Omar Trevino <omar.dintec@gmail.com>
2020-06-29 21:36:44 +0800

    added cocktail and dose module

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,30 @@
 class DosesController < ApplicationController
   def new
-    @cocktail = Cocktial.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id]
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
+      raise
     else
-      render:new
+      render :new
     end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
-
 end
-
-
-# NEED TO ADD REVIEWS TO MY FORM
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'cocktails', to: 'cocktails#index', as: :cocktails
-  get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
-  get "cocktails/:id", to: 'cocktails#show', as: :cocktail
-  post 'cocktails', to: 'cocktails#create'
-  get 'cocktails/:id/edit', to: 'cocktails#edit', as: :edit_cocktail
-  patch 'cocktails/:id', to: 'cocktails#update'
-  delete 'cocktails/:id', to: 'cocktails#destroy'
+  # get 'cocktails', to: 'cocktails#index', as: :cocktails
+  # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  # post 'cocktails', to: 'cocktails#create'
+  # get 'cocktails/:id/edit', to: 'cocktails#edit', as: :edit_cocktail
+  # patch 'cocktails/:id', to: 'cocktails#update'
+  # delete 'cocktails/:id', to: 'cocktails#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resources :doses, only: [:new, :create]
