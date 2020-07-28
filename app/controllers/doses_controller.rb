bf1fe73165f38db891050f7348902ed3252ad540
Omar Trevino <omar.dintec@gmail.com>
2020-07-28 13:58:18 +0800

    changes on the doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,7 +9,7 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktails)
       raise
     else
       render :new
